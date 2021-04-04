176300e8c8a4397784a46f05203c3484630e376a
NANXIAO1971 <xiaonan1971@gmail.com>
2021-04-05 06:54:47 +0800

    “405”

--- docker/merged_list_file.sh
+++ docker/merged_list_file.sh
@@ -116,4 +116,4 @@
 13 8,16,20 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); spnode /scripts/jd_jxd.js |ts >> /scripts/logs/jd_jxd.log 2>&1
 
 # 必须要的默认定时任务请勿删除
-50 7 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
+49 10 * * * docker_entrypoint.sh |ts >> /scripts/logs/default_task.log 2>&1
